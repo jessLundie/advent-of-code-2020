274e31e92f02b6cc6a3873728956a628b8752f31
jessLundie <39742956+jessLundie@users.noreply.github.com>
2021-09-30 20:06:25 -0700

    Revisions to D2P1 per review
    Add variables for frequently used elements, change input file to txt

--- 02/02.php
+++ 02/02.php
@@ -1,21 +1,31 @@
 <?php
 
-include "02-input.php";
+$inputs = file_get_contents('02-input.txt', true);
 
 $inputs = explode("\n", $inputs);
 
 $valid_passwords = 0;
 
+
 foreach ( $inputs as $entry ) {
 
-	$min = substr( $entry, 0, (strpos($entry, "-")) );
-	$max = substr( $entry, (strpos($entry, "-") + 1) ,((strpos($entry, " ")) - (strpos($entry, "-")) - 1));
-	$letter = substr( $entry, strpos( $entry, ":") - 1, 1);
-	$password = substr( $entry, strrpos($entry, " ") + 1 );
-	$letter_count = substr_count( $password, $letter);
+	$first_dash = strpos($entry, "-");
+	$first_semi = strpos($entry, ":");
+	$first_space = strpos($entry, " ");
+	$last_space = strrpos($entry, " ");
 
- 	if (  $letter_count >= $min && $letter_count <= $max ) {
-	 	$valid_passwords++;
- 	}
+	if ( !empty($entry) ) {
+
+		$min = substr( $entry, 0, $first_dash );
+		$max = substr( $entry, $first_dash + 1 ,$first_space - $first_dash - 1);
+		$letter = substr( $entry, $first_semi - 1, 1);
+		$password = substr( $entry, $last_space + 1 );
+		$letter_count = substr_count( $password, $letter);
+
+	 	if (  $letter_count >= $min && $letter_count <= $max ) {
+		 	$valid_passwords++;
+	 	}
+	}
 }
+
 echo "There are $valid_passwords valid passwords.";
